2cce3b30188d1a248a5f50a8e9ad1801ed5f8e4c
Ravish Kumar <ravishcistb@gmail.com>
2022-06-22 10:31:45 +0530

    Learning C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 3;
+            int total = 4 + 3;
+            Console.WriteLine("total is{0}", a + b);
+            Console.ReadLine();
+
+        }
+    }
+}
